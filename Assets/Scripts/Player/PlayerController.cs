eb48a3164ee828bc892e3e08e8570661e275c3e6
Bryan Boh <Bryanboh@gmail.com>
2022-05-29 19:52:07 +0800

    fixed teleport

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,6 +13,12 @@ public class PlayerController : MonoBehaviour
     public bool canMove = true;
     float burnTimer = 0;
 
+    public GameObject Teleport1;
+
+    public GameObject Teleport2;
+
+    public GameObject Teleport3;
+
     [SerializeField]
     float burnTick = 1;
     [SerializeField]
@@ -57,6 +63,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float idleDeathTime = 60;
     Animator animator;
+
 
     void Start()
     {
